a8708e32cbfcc3809ae642fed20af2f16d953ea5
dbennes <dbennes97@gmail.com>
2023-04-01 15:46:42 -0300

    test

---
+++ seed/src/pages/teste.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { authorizeUser, getDataFromGoogleFit, renderData } from './googleFit';
+
+function teste() {
+  const [data, setData] = useState(null);
+
+  function handleClick() {
+    authorizeUser();
+  }
+
+  function renderGoogleFitData() {
+    if (data) {
+      return renderData(data);
+    } else {
+      return <p>Clique no botão para autorizar e obter os dados do Google Fit.</p>;
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>Autorizar e obter dados do Google Fit</button>
+      {renderGoogleFitData()}
+    </div>
+  );
+}
+
+export default teste;
