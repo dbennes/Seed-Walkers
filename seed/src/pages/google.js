fe5966876c9c3aa645120b84d2e748b0e03a902e
dbennes <dbennes97@gmail.com>
2023-04-01 15:50:21 -0300

    test

--- seed/src/pages/google.js
+++ seed/src/pages/google.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { authorizeUser, getDataFromGoogleFit, renderData } from './googleFit';
 
-function teste() {
+function google() {
   const [data, setData] = useState(null);
 
   function handleClick() {
@@ -24,4 +24,4 @@ function teste() {
   );
 }
 
-export default teste;
+export default google;
